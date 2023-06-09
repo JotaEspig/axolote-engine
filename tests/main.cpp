edf6fcfc3bb95c4d266343810ff43bee9c92390a
João Vitor Espig <jotaespig@gmail.com>
2023-06-08 22:20:38 -0300

    testing changes in width, height, etc

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,7 +5,11 @@
 int main(int argc, char **argv)
 {
     std::cout << "hello world axolote engine!" << std::endl;
-    auto w = axolote::Window(argc, argv);
-    w.main_loop();
+    auto w = new axolote::Window();
+    w->title = "Testing";
+    w->width = 1000;
+    w->height = 800;
+    w->main_loop();
+    delete w;
     return 0;
 }
