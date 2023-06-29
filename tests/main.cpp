94e3d2ff656e925c19b3d46342c94f0e63540d17
João Vitor Espig <jotinha1300@gmail.com>
2023-06-29 01:17:40 -0300

    changed initial size of the window

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char **argv)
     std::cout << "hello world axolote engine!" << std::endl;
     auto w = axolote::Window();
     w.set_title("Axolote Engine Test");
-    w.set_width(800);
-    w.set_height(800);
+    w.set_width(600);
+    w.set_height(600);
     w.set_color(0x0f, 0x0f, 0x0f);
     w.main_loop();
     return 0;
