a7580c011f0a713703506f8596193bbe1fbb25ca
João Vitor Espig <jotaespig@gmail.com>
2023-07-10 01:37:16 -0300

    added other headers into main header

--- include/axolote/engine.hpp
+++ include/axolote/engine.hpp
@@ -4,3 +4,10 @@
 
 #include <axolote/window.hpp>
 #include <axolote/structs.hpp>
+#include <axolote/camera.hpp>
+#include <axolote/vao.hpp>
+#include <axolote/vbo.hpp>
+#include <axolote/ebo.hpp>
+#include <axolote/shader.hpp>
+#include <axolote/texture.hpp>
+#include <axolote/mesh.hpp>
