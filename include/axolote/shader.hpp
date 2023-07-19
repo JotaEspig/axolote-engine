7728b1c4907745f0030723a592ddfa0b20005c7b
João Vitor Espig <jotaespig@gmail.com>
2023-07-18 21:27:58 -0300

    added methods to set float, float3, float4

--- include/axolote/shader.hpp
+++ include/axolote/shader.hpp
@@ -13,6 +13,11 @@ namespace axolote
 
         void set_uniform_int(const char *uniform_name, int value);
         void set_uniform_matrix4(const char *uniform_name, glm::mat4 matrix);
+        void set_uniform_float(const char *uniform_name, float x);
+        void set_uniform_float3(const char *uniform_name,
+                                 float x, float y, float z);
+        void set_uniform_float4(const char *uniform_name,
+                                 float x, float y, float z, float w);
         void activate();
         void destroy();
     };
--- src/shader.cpp
+++ src/shader.cpp
@@ -78,8 +78,30 @@ void Shader::set_uniform_int(const char *uniform_name, int value)
 
 void Shader::set_uniform_matrix4(const char *uniform_name, glm::mat4 matrix)
 {
+    activate();
     GLuint uniform_location = glGetUniformLocation(id, uniform_name);
     glUniformMatrix4fv(uniform_location, 1, GL_FALSE, glm::value_ptr(matrix));
+}
+
+void Shader::set_uniform_float(const char *uniform_name, float x)
+{
+    activate();
+    GLuint uniform_location = glGetUniformLocation(id, uniform_name);
+    glUniform1f(uniform_location, x);
+}
+
+void Shader::set_uniform_float3(const char *uniform_name, float x, float y, float z)
+{
+    activate();
+    GLuint uniform_location = glGetUniformLocation(id, uniform_name);
+    glUniform3f(uniform_location, x, y, z);
+}
+
+void Shader::set_uniform_float4(const char *uniform_name, float x, float y, float z, float w)
+{
+    activate();
+    GLuint uniform_location = glGetUniformLocation(id, uniform_name);
+    glUniform4f(uniform_location, x, y, z, w);
 }
 
 void Shader::activate()
