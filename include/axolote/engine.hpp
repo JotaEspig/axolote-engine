30c540ca3619289331151c20c1f9227506292aa4
João Vitor Espig <jotinha1300@gmail.com>
2023-09-16 10:28:54 -0300

    fix(engine.hpp): added missing headers

--- include/axolote/engine.hpp
+++ include/axolote/engine.hpp
@@ -2,12 +2,14 @@
 
 #define GLFW_INCLUDE_NONE
 
-#include <axolote/window.hpp>
-#include <axolote/structs.hpp>
 #include <axolote/camera.hpp>
+#include <axolote/ebo.hpp>
+#include <axolote/entity.hpp>
+#include <axolote/mesh.hpp>
+#include <axolote/object2d.hpp>
+#include <axolote/shader.hpp>
+#include <axolote/structs.hpp>
+#include <axolote/texture.hpp>
 #include <axolote/vao.hpp>
 #include <axolote/vbo.hpp>
-#include <axolote/ebo.hpp>
-#include <axolote/shader.hpp>
-#include <axolote/texture.hpp>
-#include <axolote/mesh.hpp>
+#include <axolote/window.hpp>
