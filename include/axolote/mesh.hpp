70221c4cbdce65ab9858ce0f50997ef50d402666
João Vitor Espig <jotaespig@gmail.com>
2023-11-06 23:14:53 -0300

    feat(mesh): add copy move and assign operator

--- include/axolote/mesh.hpp
+++ include/axolote/mesh.hpp
@@ -36,6 +36,22 @@ public:
     **/
     Mesh();
     /**
+    * \brief Copy constructor
+    * \author João Vitor Espig (JotaEspig)
+    * \date November 06, 2023
+    * \version November 06, 2023
+    * \param mesh - Mesh object
+    **/
+    Mesh(const Mesh &mesh);
+    /**
+    * \brief Move constructor
+    * \author João Vitor Espig (JotaEspig)
+    * \date November 06, 2023
+    * \version November 06, 2023
+    * \param mesh - Mesh object
+    **/
+    Mesh(Mesh &&mesh);
+    /**
     * \brief Constructor
     * \author João Vitor Espig (JotaEspig)
     * \date October 04, 2023
@@ -46,6 +62,23 @@ public:
     **/
     Mesh(const std::vector<Vertex> &_vertices, const std::vector<GLuint> &_indices,
          const std::vector<Texture> &_textures);
+
+    /**
+    * \brief operator = overload (copy)
+    * \author João Vitor Espig (JotaEspig)
+    * \date November 06, 2023
+    * \version November 06, 2023
+    * \param mesh - Mesh object
+    **/
+    void operator=(const Mesh &mesh);
+    /**
+    * \brief operator = overload (move)
+    * \author João Vitor Espig (JotaEspig)
+    * \date November 06, 2023
+    * \version November 06, 2023
+    * \param mesh - Mesh object
+    **/
+    void operator=(Mesh &&mesh);
 
 protected:
     /** polygon mesh vertices **/
