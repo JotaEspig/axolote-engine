8464ca18a73ee9f1c3b67edcd9d6abde66411613
João Vitor Espig <jotaespig@gmail.com>
2023-11-09 20:21:57 -0300

    docs(scene.hpp): improved

--- include/axolote/scene.hpp
+++ include/axolote/scene.hpp
@@ -37,12 +37,40 @@ public:
     Scene();
     ~Scene();
 
+    /**
+     * \brief add an Entity to the scene
+     * \author João Vitor Espig (JotaEspig)
+     * \date November 08, 2023
+     * \version November 08, 2023
+    **/
     void add_drawable(Entity *e);
+    /**
+     * \brief add an Object3D to the scene
+     * \author João Vitor Espig (JotaEspig)
+     * \date November 08, 2023
+     * \version November 08, 2023
+    **/
     void add_drawable(const Object3D &m);
+    /**
+     * \brief updates the camera in the shaders
+     * \author João Vitor Espig (JotaEspig)
+     * \date November 08, 2023
+     * \version November 08, 2023
+    **/
     void update_camera(float aspect_ratio);
-    /** updates the objects position **/
+    /**
+     * \brief updates the drawables
+     * \author João Vitor Espig (JotaEspig)
+     * \date November 08, 2023
+     * \version November 08, 2023
+    **/
     void update(double time);
-    /** draws in the window **/
+    /**
+     * \brief draw the drawables
+     * \author João Vitor Espig (JotaEspig)
+     * \date November 08, 2023
+     * \version November 08, 2023
+    **/
     void render();
 
 private:
