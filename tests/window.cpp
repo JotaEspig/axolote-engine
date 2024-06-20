4b4bab29869620adbec449997e7992dd65b20393
João Vitor Espig <jotaespig@gmail.com>
2024-06-20 19:56:27 -0300

    tests: added more tests

---
+++ tests/window.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include <axolote/engine.hpp>
+
+class App : public axolote::Window {
+public:
+    void main_loop();
+};
+
+void App::main_loop() {
+    // You must set a scene for the engine renders, otherwise it will give you a
+    // segfault
+    std::shared_ptr<axolote::Scene> scene{new axolote::Scene{}};
+    set_scene(scene);
+
+    float delta_t = 1.0f / 60.0f;
+    while (!should_close()) {
+        // Updating the camera and the scene objects
+        update_camera((float)width() / height());
+        update(delta_t);
+
+        // Clearing the screen and rendering the scene
+        clear();
+        render();
+
+        // Swapping the buffers MUST BE CALLED, otherwise bye bye PC :)
+        flush();
+    }
+}
+
+int main() {
+    std::cout << "Axolote Engine" << std::endl;
+    App app{};
+    app.set_title("Window");
+    app.set_width(600);
+    app.set_height(600);
+    app.set_color(0x01, 0x40, 0xff);
+    app.main_loop();
+    return 0;
+}
