71d43e6ecfab82f8a74eba856e34be5da654ad63
João Vitor Espig <jotaespig@gmail.com>
2024-07-26 20:05:31 -0300

    feat: add debug print function

--- include/axolote/utils.hpp
+++ include/axolote/utils.hpp
@@ -15,5 +15,28 @@ namespace axolote {
  * \param filename - name of file
  **/
 std::string get_file_content(const char *filename);
+#ifdef DEBUG
+/**
+ * \brief Debug function to print
+ * \author João Vitor Espig (JotaEspig)
+ *
+ * This function is a debug function to print to stdout. Works like printf.
+ **/
+template <typename... Args>
+void debug(const char *format, Args &&...args) {
+    std::string new_format = std::string("DEBUG: ") + format;
+    printf(new_format.c_str(), std::forward<Args>(args)...);
+}
+#else
+/**
+ * \brief Debug function to print
+ * \author João Vitor Espig (JotaEspig)
+ *
+ * DEBUG is not defined, so this function does nothing.
+ **/
+template <typename... Args>
+void debug(const char *format, Args &&...args) {
+}
+#endif
 
 } // namespace axolote
