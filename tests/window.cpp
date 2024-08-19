1383259bb1c86681b2a4eb9444b8597a3e9774ac
João Vitor Espig <jotaespig@gmail.com>
2024-08-19 12:22:57 -0300

    docs: updated examples

--- tests/window.cpp
+++ tests/window.cpp
@@ -8,13 +8,17 @@ public:
 };
 
 void App::main_loop() {
-    // You must set a scene for the engine to render it, otherwise it will give you a
-    // segfault
+    // You must set a scene for the engine to render it, otherwise it will give
+    // you a segfault
     std::shared_ptr<axolote::Scene> scene{new axolote::Scene{}};
     set_scene(scene);
 
     float delta_t = 1.0f / 60.0f;
     while (!should_close()) {
+        // Polling events and processing input
+        poll_events();
+        process_input();
+
         // Updating the camera and the scene objects
         update_camera((float)width() / height());
         update(delta_t);
