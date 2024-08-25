f2d5811df6b5f82fb5992ad36c03223e761d1f56
João Vitor Espig <jotaespig@gmail.com>
2024-08-25 02:48:56 -0300

    starting framebuffer

--- include/axolote/gl/framebuffer.hpp
+++ include/axolote/gl/framebuffer.hpp
@@ -23,10 +23,35 @@ class Framebuffer : public OpenGLObject {
 public:
     template <typename... Args>
     static std::shared_ptr<Framebuffer> create(Args &&...args);
+    /**
+     * @brief id getter
+     * @author João Vitor Espig (jotaespig@gmail.com)
+     **/
+    GLuint id() const override;
+    /**
+     * @brief Initializes the framebuffer with a given width and height
+     * @author João Vitor Espig (jotaespig@gmail.com)
+     **/
+    void init(std::uint16_t width, std::uint16_t height);
+    /**
+     * @brief Bind the framebuffer to the OpenGL context
+     * @author João Vitor Espig (jotaespig@gmail.com)
+     **/
+    void bind();
+    /**
+     * @brief Unbind the framebuffer from the OpenGL context
+     * @author João Vitor Espig (jotaespig@gmail.com)
+     **/
+    void unbind();
+    /**
+     * @brief destroys
+     * @author João Vitor Espig (jotaespig@gmail.com)
+     **/
+    void destroy() override;
 
 private:
     struct Deleter {
-        void operator()(Framebuffer *framebuffer);
+        void operator()(Framebuffer *fbo);
     };
 
     GLuint _id;
--- src/gl/framebuffer.cpp
+++ src/gl/framebuffer.cpp
@@ -5,9 +5,66 @@ namespace axolote {
 
 namespace gl {
 
+GLuint Framebuffer::id() const {
+    return _id;
+}
+
+void Framebuffer::init(std::uint16_t width, std::uint16_t height) {
+    bind();
+    _texture = Texture::create();
+    _texture->bind();
+    GLuint tex_id = _texture->id();
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE,
+        NULL
+    );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glFramebufferTexture2D(
+        GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex_id, 0
+    );
+
+    glGenRenderbuffers(1, &_rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, _rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    glFramebufferRenderbuffer(
+        GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbo
+    );
+
+    bool is_ready
+        = glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE;
+    if (!is_ready) {
+        debug("Framebuffer %u is not complete!", _id);
+    }
+
+    unbind();
+    _texture->unbind();
+}
+
+void Framebuffer::bind() {
+    glBindFramebuffer(GL_FRAMEBUFFER, _id);
+}
+
+void Framebuffer::unbind() {
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void Framebuffer::destroy() {
+    GLuint id = _id;
+    glDeleteFramebuffers(1, &_id);
+    debug("Framebuffer destroyed: %u", id);
+}
+
 Framebuffer::Framebuffer() {
     glGenFramebuffers(1, &_id);
     debug("Framebuffer created: %u", _id);
+}
+
+void Framebuffer::Deleter::operator()(Framebuffer *fbo) {
+    fbo->destroy();
+    delete fbo;
 }
 
 } // namespace gl
