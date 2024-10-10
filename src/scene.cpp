dfac098abd29a2bf396220c098ff5f5ab890daa1
João Vitor Espig <jotaespig@gmail.com>
2024-10-09 22:46:27 -0300

    fix: ambient color

--- src/scene.cpp
+++ src/scene.cpp
@@ -174,14 +174,19 @@ void Scene::update(double delta_t) {
     );
     for (auto &o : all_objects) {
         for (auto &shader : o->get_shaders()) {
-            shader->set_uniform_float("axolote_ambient_light", 1);
+            shader->set_uniform_float3(
+                "axolote_ambient_light", ambient_light.x,
+                ambient_light.y, ambient_light.z
+            );
             shader->set_uniform_float(
-                    "axolote_ambient_light_intensity", ambient_light_intensity
-                    );
-            shader->set_uniform_int("axolote_num_point_lights", num_point_lights);
+                "axolote_ambient_light_intensity", ambient_light_intensity
+            );
             shader->set_uniform_int(
-                    "axolote_num_directional_lights", num_directional_lights
-                    );
+                "axolote_num_point_lights", num_point_lights
+            );
+            shader->set_uniform_int(
+                "axolote_num_directional_lights", num_directional_lights
+            );
             shader->set_uniform_int("axolote_num_spot_lights", num_spot_lights);
         }
     }
