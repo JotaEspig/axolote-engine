59ed1323ce6881571f07f759e2ea768fe088c4fb
João Vitor Espig <jotaespig@gmail.com>
2024-10-19 18:58:28 -0300

    refactor: stopped using alias

--- install_windows.ps1
+++ install_windows.ps1
@@ -1,41 +1,44 @@
 if ($args.Length -eq 0) {
-    echo "This script copies the shared library needed to use Axolote Engine"
-    echo "and paste into a path of your choice. It creates the folder"
-    echo "'resources' containing ONLY the shaders and creates the folder"
-    echo "'external' that has subfolders 'lib' and 'include'."
-    echo ""
-    echo "Usage: powershell -ExecutionPolicy Bypass -File .\install_windows.ps1 <path-to-install>"
+    Write-Output "This script copies the shared library needed to use Axolote Engine"
+    Write-Output "and paste into a path of your choice. It creates the folder"
+    Write-Output "'resources' containing ONLY the shaders and creates the folder"
+    Write-Output "'external' that has subfolders 'lib' and 'include'."
+    Write-Output ""
+    Write-Output "Usage: powershell -ExecutionPolicy Bypass -File .\install_windows.ps1 <path-to-install>"
     exit 1
 }
 
-$path=$args[0]
+$path = $args[0]
 if (-not (Test-Path $path)) {
-    echo "target path not found"
+    Write-Output "target path not found"
     exit 1
 }
 
 # prompt to choose your build type
 $type = ""
 while ($true) {
-    echo "Choose your build type"
-    echo "Release [1]"
-    echo "Debug [2]"
-    echo "Exit script [3]"
+    Write-Output "Choose your build type"
+    Write-Output "Release [1]"
+    Write-Output "Debug [2]"
+    Write-Output "Exit script [3]"
     $option = Read-Host "Your choice"
     if ($option -ne "1" -and $option -ne "2" -and $option -ne "3") {
-        echo "Invalid option"
-        sleep 1
-        echo ""
-    } else {
+        Write-Output "Invalid option"
+        Start-Sleep -Seconds 1
+        Write-Output ""
+    }
+    else {
         break
     }
 }
 
 if ($option -eq "1") {
     $type = "Release"
-} elseif ($option -eq "2") {
+}
+elseif ($option -eq "2") {
     $type = "Debug"
-} elseif ($option -eq "3") {
+}
+elseif ($option -eq "3") {
     exit 1
 }
 
@@ -43,9 +46,9 @@ $vcpkg_path = ""
 while ($true) {
     $vcpkg_path = Read-Host "Enter the path to vcpkg.cmake"
     if (-not (Test-Path $vcpkg_path)) {
-        echo "File not found. Try again"
-        sleep 1
-        echo ""
+        Write-Output "File not found. Try again"
+        Start-Sleep -Seconds 1
+        Write-Output ""
     }
     else {
         break
@@ -53,30 +56,30 @@ while ($true) {
 }
 
 if (-not (Test-Path "build")) {
-    mkdir build
+    New-Item -ItemType Directory -Name build
 }
-cd build
-cmake -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=$path_to_vcpkg ..
+Set-Location build
+cmake -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=$vcpkg_path ..
 cmake --build . --config $type
-cd ..
+Set-Location ..
 
-echo ""
-echo "Creating temporary folder"
-mkdir tmp/external/lib/axolote
-mkdir tmp/external/include
-mkdir tmp/external/imgui
-mkdir tmp/resources/shaders
+Write-Output ""
+Write-Output "Creating temporary folder"
+New-Item -ItemType Directory -Path tmp/external/lib/axolote
+New-Item -ItemType Directory -Path tmp/external/include
+New-Item -ItemType Directory -Path tmp/external/imgui
+New-Item -ItemType Directory -Path tmp/resources/shaders
 
-echo "Copying content to temporary folder"
-cp build/$type/* tmp/external/lib/axolote
-cp -r include/axolote tmp/external/include
-cp -r external/imgui/* tmp/external/imgui/
-cp -r external/include/KHR tmp/external/include
-cp -r resources/shaders/* tmp/resources/shaders
+Write-Output "Copying content to temporary folder"
+Copy-Item -Path build/$type/* -Destination tmp/external/lib/axolote
+Copy-Item -Recurse -Path include/axolote -Destination tmp/external/include
+Copy-Item -Recurse -Path external/imgui/* -Destination tmp/external/imgui/
+Copy-Item -Recurse -Path external/include/KHR -Destination tmp/external/include
+Copy-Item -Recurse -Path resources/shaders/* -Destination tmp/resources/shaders
 
-echo "Moving to target path: $path"
-cp -r tmp/* $path
+Write-Output "Moving to target path: $path"
+Copy-Item -Recurse -Path tmp/* -Destination $path
 
-echo "Cleaning temp folder"
+Write-Output "Cleaning temp folder"
 Remove-Item -Recurse -Force tmp
 exit 0
